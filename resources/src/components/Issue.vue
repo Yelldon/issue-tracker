fe46f59943476a2220391c69de0faacf38576fe3
Greg Patrick <greg.hello@gmail.com>
2019-05-29 16:45:19 -0500

    Update basic styling

--- resources/src/components/Issue.vue
+++ resources/src/components/Issue.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="border border-gray-500 rounded mb-4 p-4">
-    <p>{{ issue.id }}</p>
-    <p>{{ issue.title }}</p>
-    <p>{{ issue.text }}</p>
-    <p v-if="issue.user">Created By: {{ issue.user.firstname }} {{ issue.user.lastname }}</p>
-    <p>------------</p>
+  <div class="flex border border-gray-500 bg-white rounded mb-4 p-4">
+    <div class="w-1/2">
+      <p>Issue # {{ issue.id }}</p>
+      <p>{{ issue.title }}</p>
+      <p>{{ issue.text }}</p>
+    </div>
+    <div class="w-1/2">
+      <p v-if="issue.user">Created By: {{ issue.user.firstname }} {{ issue.user.lastname }}</p>
+      <p>Created By: {{ issue.createdAt }}</p>
+      <p>Updated By: {{ issue.updatedAt }}</p>
+    </div>
   </div>
 </template>
 
